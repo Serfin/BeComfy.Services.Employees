53ad8ff8c1c061b970052fddf272e3c945baee49
serfin <serfin009@gmail.com>
2020-03-07 00:50:09 +0100

    Added .net core file structure

--- src/BeComfy.Services.Employees.Api/Startup.cs
+++ src/BeComfy.Services.Employees.Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace BeComfy.Services.Employees
+namespace BeComfy.Services.Employees.Api
 {
     public class Startup
     {
